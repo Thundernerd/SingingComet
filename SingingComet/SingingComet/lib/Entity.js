97f0d4f9dc8e95def9fc6d0a90d702fa9ec98aba
Christiaan Bloemendaal <chrisbloemendaal@hotmail.com>
2015-01-16 16:55:37 +0100

    Updated gitignore, Entity

--- SingingComet/SingingComet/lib/Entity.js
+++ SingingComet/SingingComet/lib/Entity.js
@@ -1,9 +1,12 @@
 var Entity = (function () {
-    function Entity() {
+    function Entity(x, y) {
+        if (x === void 0) { x = 0; }
+        if (y === void 0) { y = 0; }
+        this.Position = new Vector2(x, y);
     }
     Entity.prototype.Update = function () {
     };
-    Entity.prototype.Draw = function () {
+    Entity.prototype.Draw = function (ctx) {
     };
     return Entity;
 })();
--- SingingComet/SingingComet/lib/Entity.ts
+++ SingingComet/SingingComet/lib/Entity.ts
@@ -1,12 +1,16 @@
 ﻿class Entity {
 
-    constructor() { }
+    Position: Vector2;
+
+    constructor(x:number = 0, y:number = 0) {
+        this.Position = new Vector2(x, y);
+    }
 
     Update() {
 
     }
 
-    Draw() {
+    Draw(ctx:CanvasRenderingContext2D) {
 
     }
 }
