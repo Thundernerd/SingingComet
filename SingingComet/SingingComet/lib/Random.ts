e04c115bb3dd43775bc9b1f70fb49cbd208edb02
Christian Kokott <christian.kokott@gmail.com>
2015-01-18 20:34:04 +0100

    added Random- and Canvas Helper functions

---
+++ SingingComet/SingingComet/lib/Random.ts
@@ -0,0 +1,25 @@
+﻿function RandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function RandomNumber(min, max) {
+    return Math.random() * (max - min + 1) + min;
+}
+
+function RandomRGB(alpha = 1) {
+    var r = 255 * Math.random() | 0,
+        g = 255 * Math.random() | 0,
+        b = 255 * Math.random() | 0;
+
+    return 'rgba(' + r + ',' + g + ',' + b + ',' + alpha + ')';
+}
+
+function RandomRGBA() {
+    var r = 255 * Math.random() | 0,
+        g = 255 * Math.random() | 0,
+        b = 255 * Math.random() | 0,
+        a = 255 * Math.random() | 0;
+
+    return 'rgba(' + r + ',' + g + ',' + b + ',' + a + ')';
+}
+
